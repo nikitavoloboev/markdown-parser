68a06e8b05b117fd3db0147419bb041d434b4348
Nikita Voloboev <nikita.voloboev@icloud.com>
2017-10-29 11:49:57 +0100

    everything works

--- main.go
+++ main.go
@@ -1,23 +1,55 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
 )
 
-//  downloadMD given URL, downloads Markdown file from the URL.
-func downloadMD() {
-
+// DownloadURL returns Body response from the URL.
+func DownloadURL(URL string) string {
+	response, err := http.Get(URL)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		defer response.Body.Close()
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(response.Body)
+		return buf.String()
+	}
+	return "test"
 }
 
-//  getAllLinks returns all links and their names from a given markdown file.
-func getAllLinks(fileContent string) {
+// GetAllLinks returns all links and their names from a given markdown file.
+func GetAllLinks(markdown string) map[string]string {
+	// holds all the links and their corresponding values
+	m := make(map[string]string)
 
+	// regex to extract link and text attached to link
+	re := regexp.MustCompile(`\[([^\]]*)\]\(([^)]*)\)`)
+
+	scanner := bufio.NewScanner(strings.NewReader(markdown))
+	// scans line by line
+	for scanner.Scan() {
+		// apply regex
+		matches := re.FindAllStringSubmatch(scanner.Text(), -1)
+
+		// only apply regex if there are links
+		if matches != nil {
+			m[matches[0][1]] = matches[0][2]
+		}
+	}
+	return m
 }
 
-// readFile Reads file.
+// readFile returns contents of the file.
 func readFile(filename string) string {
-	b, err := ioutil.ReadFile(filename) // just pass the file name
+	b, err := ioutil.ReadFile(filename)
 	if err != nil {
 		fmt.Print(err)
 	}
@@ -25,6 +57,7 @@ func readFile(filename string) string {
 }
 
 func main() {
-	fileContent := readFile("Summary.md")
-	getAllLinks(fileContent)
+	markdownFile := DownloadURL("https://raw.githubusercontent.com/sindresorhus/awesome/master/readme.md")
+	result := GetAllLinks(markdownFile)
+	fmt.Println(result["Spanish"])
 }
