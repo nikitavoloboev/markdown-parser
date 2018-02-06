04d5f8adfe874dda4889e49f8c2677c6f4c6b641
Nikita Voloboev <nikita.voloboev@icloud.com>
2018-02-06 17:29:25 +0100

    Remove websites.md

--- parser/parser.go
+++ parser/parser.go
@@ -53,21 +53,33 @@ func startsWithHash(line string) bool {
 	return strings.HasPrefix(line, "#")
 }
 
+// ParseLinkWithDescription parses a line and grabs the Link, Title and the Description attached to it.
+// The format of the line should be as follows: `- [Title](Link) - Description.
+// Description can be omitted.
+// TODO: test it
+func ParseLinkWithDescription(line string) {
+	re := regexp.MustCompile(`\[([^]]+)\]\(([^)]+)\)(.*)`)
+	match := re.FindStringSubmatch(line)
+	if len(match) != 0 {
+		fmt.Printf(match[0])
+	}
+}
+
 // GetAllLinks returns all links and their names from a given markdown file.
 func GetAllLinks(markdown string) map[string]string {
-	// holds all the links and their corresponding values
+	// Holds all the links and their corresponding values
 	m := make(map[string]string)
 
-	// regex to extract link and text attached to link
+	// Regex to extract link and text attached to link
 	re := regexp.MustCompile(`\[([^\]]*)\]\(([^)]*)\)`)
 
 	scanner := bufio.NewScanner(strings.NewReader(markdown))
-	// scans line by line
+	// Scans line by line
 	for scanner.Scan() {
-		// make regex
+		// Make regex
 		matches := re.FindAllStringSubmatch(scanner.Text(), -1)
 
-		// only apply regex if there are links and the link does not start with #
+		// Only apply regex if there are links and the link does not start with #
 		if matches != nil {
 			if startsWithHash(matches[0][2]) == false {
 				// fmt.Println(matches[0][2])
@@ -114,139 +126,3 @@ func readFile(filename string) string {
 	}
 	return string(b)
 }
-
-// TODO: transfer whats below
-// package main
-
-// import (
-// 	"bufio"
-// 	"encoding/csv"
-// 	"encoding/json"
-// 	"fmt"
-// 	"github.com/urfave/cli"
-// 	"log"
-// 	"os"
-// 	"regexp"
-// 	"strings"
-// )
-
-// type Link struct {
-// 	Title        string `json:"title"`
-// 	Arg          string `json:"arg"`
-// 	Autocomplete string `json:"autocomplete"`
-// }
-
-// type Items struct {
-// 	Items []Link `json:"items"`
-// }
-
-// func mdToJSON(fileName string) {
-
-// 	file, err := os.Open(fileName)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer file.Close()
-// 	var results Items
-
-// 	s := bufio.NewScanner(file)
-
-// 	re := regexp.MustCompile(`^[#-]+ \[([^\]]+)\]\(([^\)]+)`)
-// 	for s.Scan() {
-// 		ln := s.Text()
-
-// 		matches := re.FindAllStringSubmatch(ln, -1)
-// 		if len(matches) != 1 {
-// 			continue
-// 		}
-// 		results.Items = append(results.Items, Link{
-// 			Title:        matches[0][1],
-// 			Arg:          matches[0][2],
-// 			Autocomplete: matches[0][1],
-// 		})
-// 	}
-
-// 	if err := s.Err(); err != nil {
-// 		log.Fatalln("could not scan input", err)
-// 	}
-
-// 	out, err := json.Marshal(results)
-// 	if err != nil {
-// 		log.Fatalln("could not marshal result", err)
-// 	}
-
-// 	f, err := os.Create(strings.Replace(fileName, ".md", ".json", -1))
-// 	if err != nil {
-// 		log.Fatalln("could not make output file", err)
-// 	}
-// 	defer f.Close()
-// 	f.WriteString(string(out))
-// 	fmt.Println(strings.Replace(fileName, ".md", ".json", -1) + " was created")
-// }
-
-// // TODO: clean it up and export it to + test + view the local readme I have for instructions (document things)
-// func mdToCSV(fileName string) {
-// 	file, err := os.Open(fileName)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer file.Close()
-// 	out := csv.NewWriter(file)
-// 	re := regexp.MustCompile(`^- \[([^\]]+)\]\(([^)]+)`)
-
-// 	s := bufio.NewScanner(file)
-// 	for s.Scan() {
-// 		ln := s.Text()
-
-// 		if !re.MatchString(ln) {
-// 			continue
-// 		}
-// 		matches := re.FindAllStringSubmatch(ln, -1)
-
-// 		row := []string{
-// 			matches[0][1],
-// 			"",
-// 			matches[0][2],
-// 		}
-// 		if err := out.Write(row); err != nil {
-// 			log.Print(err)
-// 		}
-// 	}
-// 	if err := s.Err(); err != nil {
-// 		log.Fatalln("scan error", err)
-// 	}
-
-// 	out.Flush()
-// 	if err := out.Error(); err != nil {
-// 		log.Fatalln("write error", err)
-// 	}
-
-// 	// write to file
-// 	f, err := os.Create(strings.Replace(fileName, ".md", ".csv", -1))
-// 	if err != nil {
-// 		log.Fatalln("could not make output file", err)
-// 	}
-// 	defer f.Close()
-
-// 	fmt.Println("alfred.csv file was created")
-// }
-
-// func main() {
-
-// 	// read options
-// 	app := cli.NewApp()
-// 	app.Action = func(c *cli.Context) error {
-// 		action := c.Args().Get(0)
-// 		fileName := c.Args().Get(1)
-// 		switch action {
-// 		case "json":
-// 			mdToJSON(fileName)
-// 		case "csv":
-// 			mdToCSV(fileName)
-// 		}
-// 		return nil
-// 	}
-
-// 	app.Run(os.Args)
-
-// }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -6,6 +6,12 @@ import (
 	"testing"
 )
 
+func TestParseLinkWithDescription(t *testing.T) {
+	v := "- [The art of computer programming (1968)](http://www.goodreads.com/book/show/112239.The_Art_of_Computer_Programming_Volumes_1_3_Boxed_Set)"
+	ParseLinkWithDescription(v)
+	// result := TestParseLinkWithDescription(v)
+}
+
 func TestStartsWithHash(t *testing.T) {
 	v := "# Intro"
 	result := startsWithHash(v)
