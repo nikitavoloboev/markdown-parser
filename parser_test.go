64dd9c412f9db485f6b5df5245f1449d5d5e879e
John Letey <john.letey@colorado.edu>
2019-02-19 18:31:17 +0000

    refactor: just one test case

--- parser_test.go
+++ parser_test.go
@@ -7,22 +7,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseLinkWithDescription(t *testing.T) {
+func TestParseLink(t *testing.T) {
+	// Define assertion
 	assert := assert.New(t)
+	// Link with description
 	v := "- [Effective Go](https://golang.org/doc/effective_go.html) - Amazing doc."
 	result := ParseLink(v)
 	assert.Equal(result["Title"], "Effective Go", "Check to see if the title is correct")
 	assert.Equal(result["Link"], "https://golang.org/doc/effective_go.html", "Check to see if the link is correct")
 	assert.Equal(result["Description"], "Amazing doc.", "Check to see if the description is correct")
-}
-
-func TestParseLinkWithoutDescription(t *testing.T) {
-	assert := assert.New(t)
-	v := "- [Effective Go](https://golang.org/doc/effective_go.html)"
-	result := ParseLink(v)
-	assert.Equal(result["Title"], "Effective Go", "Check to see if the title is correct")
-	assert.Equal(result["Link"], "https://golang.org/doc/effective_go.html", "Check to see if the link is correct")
-	assert.Equal(result["Description"], "", "Check to see if the description is correct")
+	// Link without description
+	v2 := "- [Effective Go](https://golang.org/doc/effective_go.html)"
+	result2 := ParseLink(v2)
+	assert.Equal(result2["Title"], "Effective Go", "Check to see if the title is correct")
+	assert.Equal(result2["Link"], "https://golang.org/doc/effective_go.html", "Check to see if the link is correct")
+	assert.Equal(result2["Description"], "", "Check to see if the description is correct")
 }
 
 func TestParseMarkdownFile(t *testing.T) {
